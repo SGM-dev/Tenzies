0e7ffd22dfdc5bfe126cfcf12365e76876fdb542
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-10-17 23:48:12 +0530

    use fontAwesome to render dots on the die

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDiceOne,
+  faDiceTwo,
+  faDiceThree,
+  faDiceFour,
+  faDiceFive,
+  faDiceSix,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Die(props) {
   const styles = {
-    backgroundColor: props.isHeld ? "#59E391" : "#fff",
+    color: props.isHeld ? "#000" : "#b1b1b1",
+  };
+
+  const dieHtml = {
+    1: <FontAwesomeIcon icon={faDiceOne} className="fa-3x" style={styles} />,
+    2: <FontAwesomeIcon icon={faDiceTwo} className="fa-3x" style={styles} />,
+    3: <FontAwesomeIcon icon={faDiceThree} className="fa-3x" style={styles} />,
+    4: <FontAwesomeIcon icon={faDiceFour} className="fa-3x" style={styles} />,
+    5: <FontAwesomeIcon icon={faDiceFive} className="fa-3x" style={styles} />,
+    6: <FontAwesomeIcon icon={faDiceSix} className="fa-3x" style={styles} />,
   };
 
   return (
-    <div className="die-face" style={styles} onClick={props.holdDice}>
-      <h2 className="die-num ">{props.value}</h2>
+    <div className="die-face" onClick={props.holdDice}>
+      {dieHtml[props.value]}
     </div>
   );
 }
